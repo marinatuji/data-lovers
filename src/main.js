5696d641df462f8592a3bae378ccbdf1a6000eae
marinatuji <mtujimura@gmail.com>
2019-03-11 06:30:46 -0300

    Adicionada Funcão Ordenar. CSS parcial do Filtro

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ function showNews() {
 }
 
 function parseDate(date) {
-  return new Date(date * 1000);
+  return new Date(date * 1000).toLocaleDateString("pt-br");
 }
 
 function filterNewsByDate(date) { //validar data
@@ -30,20 +30,22 @@ function filterNewsByDate(date) { //validar data
 
 function filterNewsByTitle(title) { //validar dados se existe
   return getData().filter(eachNews => {
-    return parseDate(eachNews["title"]) === title;
+    return eachNews["title"] === title;
   });
-}
+}//procurar busca parcial, contem string
 
-addEventListener("click", function () {
-  let valueInputDate = document.getElementById("idDoInput").value;
-  let valueInputTitle = document.getElementById("idDoInput").value;
+const btnFilter = document.querySelector("input[name='filter']");
+btnFilter.addEventListener("click", function (event) {
+  event.preventDefault();
+  let valueInputDate = document.querySelector("input[name='byDate']").value;
+  let valueInputTitle = document.querySelector(".input-by-title").value;
   let newArray = [];
   if (valueInputDate) {
     newArray = filterNewsByDate(valueInputDate);
   } else if (valueInputTitle) {
     newArray = filterNewsByTitle(valueInputTitle);
   }
-  showResult(newArray);
+  filterNews(newArray);
 });
 
 function showResult(filterNews) {
@@ -57,5 +59,24 @@ function showResult(filterNews) {
   `
 }
 
-function orderByDate() {//challenge
+function filterNews(filterNews) {
+  let filteredNews = document.querySelector("#show-filtered-news");
+  filteredNews.innerHTML = `
+    ${filterNews.map((eachNews) => `
+      <ul class="all-news">
+        <li>${eachNews["title"]} / ${eachNews["feedlabel"]}</li>
+      </ul>
+      `).join("")}
+  `
+}
+
+function sortNewest() {//challenge
+  return getData().sort((a, b) => {
+    if (parseDate(b["date"]) < parseDate(a["date"])) {
+      return -1;
+    } if (parseDate(a["date"]) > parseDate(b["date"])) {
+      return 1;
+    }
+    return 0;
+  });
 }
